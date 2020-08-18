9638ca4fdd1596de1836c5416d58a24747de5db1
tylermcleant <tylermcleant@aol.com>
2020-08-18 15:08:13 -0400

    Added some Psuedo code

--- guessinggame.py
+++ guessinggame.py
@@ -1,9 +1,10 @@
-#finding the 
+#Calling the 'random property to enable to system to randomly choose a number between one and ten. 
 import random
 number = random.randint(1, 10)
-
+#Asking the user to input a name so the system can identify user. Also the counter is set to 'zero' to characterize the guesses allowed.  
 player_name = input("Hello, What's your name?")
 number_of_guesses = 0
+#System will output/display the users name and querying the user to enter a number between one and ten. 
 print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
 
 while number_of_guesses < 5:
